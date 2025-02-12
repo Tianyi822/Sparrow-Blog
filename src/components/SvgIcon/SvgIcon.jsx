259f27bc8e5ce5bb59da1e4e14b4dd3d2b840681
Tianyi <chentyit@163.com>
2025-02-12 16:42:45 +0800

    feat: 新增 SvgIcon 组件统一管理图标
    新增可复用的 SvgIcon 组件，用于统一管理项目中的 SVG 图标：
    
    - 支持三种预设尺寸：small、normal、large
    - 支持通过 props 动态控制颜色
    - 支持图标悬停动画效果
    - 添加基础图标：搜索、首页、时间轴、标签、分类、友链、关于
    - 清理 SVG 文件，移除默认样式和颜色
    - 使用 vite-plugin-svgr 实现 SVG 到 React 组件的转换
    
    该组件提供了一种统一的图标处理方式，使得整个应用的图标样式更易于维护和统一管理。

---
+++ src/components/SvgIcon/SvgIcon.jsx
@@ -0,0 +1,85 @@
+import './SvgIcon.scss'
+import PropTypes from 'prop-types';
+import classNames from "classnames";
+import SearchSvg from '@/assets/svgs/search.svg?react'
+import HomeSvg from '@/assets/svgs/home.svg?react'
+import TimeLineSvg from '@/assets/svgs/timeLine.svg?react'
+import FriendLinkSvg from '@/assets/svgs/friendLink.svg?react'
+import AboutSvg from '@/assets/svgs/about.svg?react'
+import TagSvg from '@/assets/svgs/tag.svg?react'
+import CategorySvg from '@/assets/svgs/category.svg?react'
+import { Fragment } from "react";
+
+// 图标尺寸
+export const Normal = "normal"
+export const Small = "small"
+export const Large = "large"
+
+// 图标名称
+export const Search = "search"
+export const Home = "home"
+export const TimeLine = "timeLine"
+export const FriendLink = "friendLink"
+export const About = "about"
+export const Tag = "tag"
+export const Category = "category"
+
+
+const SvgIcon = (props) => {
+    const {
+        size = Normal,  // 默认尺寸
+        color = 'currentColor', // 默认颜色跟随父元素
+        name, // 图标名称
+        className = '' // 自定义类名
+    } = props
+
+    const clsName = classNames('svg-icon', className, {
+        [`svg-icon-${size}`]: size
+    })
+
+    let icon = <div/>
+    switch (name) {
+        case Search:
+            icon = <SearchSvg className={clsName} stroke={color}/>
+            break;
+        case Home:
+            icon = <HomeSvg className={clsName} stroke={color}/>
+            break;
+        case TimeLine:
+            icon = <TimeLineSvg className={clsName} stroke={color}/>
+            break;
+        case FriendLink:
+            icon = <FriendLinkSvg className={clsName} stroke={color}/>
+            break;
+        case About:
+            icon = <AboutSvg className={clsName} stroke={color}/>
+            break;
+        case Tag:
+            icon = <TagSvg className={clsName} stroke={color}/>
+            break;
+        case Category:
+            icon = <CategorySvg className={clsName} stroke={color}/>
+            break;
+        default:
+            console.error("WARNING: 图标名称错误")
+            break;
+    }
+
+    return (
+        <Fragment>
+            {icon}
+        </Fragment>
+    )
+}
+
+SvgIcon.propTypes = {
+    size: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ]),
+    color: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    className: PropTypes.string
+};
+
+export default SvgIcon;
