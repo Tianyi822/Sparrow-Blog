7f74dfed009eae0c7517129cac5546df086fa020
Tianyi <chentyit@163.com>
2025-02-14 12:55:56 +0800

    refactor: 重构项目结构和组件职责 - 创建独立的Home组件管理主页面内容和状态 - 将背景图片状态从App移至Home组件 - 迁移相关样式到Home.scss - 清理App组件中的冗余代码

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect, Fragment } from "react";
 import "./App.scss"
 import Navigator from "@/components/Navigator/Navigator.jsx";
-import Introduction from "@/components/Introduction/Introduction.jsx";
-import SvgIcon, { DownArrow, Large } from "./components/SvgIcon/SvgIcon";
+import Home from "@/components/Home/Home.jsx";
 
 function App() {
     const [navIndex, setNavIndex] = useState(1);
-    // 模拟从后端获取的背景图片地址
-    const [bgImage] = useState("https://easy-blog-test.oss-cn-guangzhou.aliyuncs.com/images/background_image.webp");
 
     useEffect(() => {
         document.title = "Tianyi's Blog";
@@ -16,16 +13,7 @@ function App() {
     return (
         <Fragment>
             <Navigator className="app-navigator" index={navIndex} setIndex={setNavIndex}/>
-            <div className="app-container">
-                <div className="bg-image" style={{backgroundImage: `url(${bgImage})`}}></div>
-                <div className="bg-overlay"></div>
-                <Introduction className={"app-introduction"}/>
-                <SvgIcon 
-                    name={DownArrow} 
-                    size={Large}
-                    className="app-down-arrow"
-                />
-            </div>
+            <Home />
         </Fragment>
     )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import { Fragment, useState } from "react";
+import Introduction from "@/components/Introduction/Introduction.jsx";
+import SvgIcon, { DownArrow, Large } from "@/components/SvgIcon/SvgIcon";
+import "./Home.scss";
+
+const Home = () => {
+    // 将背景图片状态移到Home组件
+    const [bgImage] = useState("https://easy-blog-test.oss-cn-guangzhou.aliyuncs.com/images/background_image.webp");
+
+    return (
+        <div className="home">
+            <div className="bg-image" style={{backgroundImage: `url(${bgImage})`}}></div>
+            <div className="bg-overlay"></div>
+            <Introduction className="app-introduction"/>
+            <SvgIcon 
+                name={DownArrow} 
+                size={Large}
+                className="app-down-arrow"
+            />
+        </div>
+    );
+};
+
+export default Home; 
