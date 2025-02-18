9e704c31a9dc502ddbd473aafdefb601ed7a8c12
Tianyi <chentyit@163.com>
2025-02-18 21:05:57 +0800

    refactor: 删除档案馆所有组件，功能冗余了

--- src/components/ArchiveRoom/types.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const BlogType = PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string.isRequired,
-}); 
