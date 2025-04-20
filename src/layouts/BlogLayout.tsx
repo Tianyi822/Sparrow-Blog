0b854ccab3742aa8b7bcb4dc9843dbd32fdf242b
Tianyi <chentyit@163.com>
2025-04-20 11:33:27 +0800

    feat(layout): 动态获取博客用户信息
    - 添加获取用户信息的功能，包括用户名、背景图片、网站标题和图标
    - 更新导航栏组件，传递用户名作为 props
    - 优化背景图片的加载逻辑，只有在图片存在时才渲染 Background 组件

--- src/layouts/BlogLayout.tsx
+++ src/layouts/BlogLayout.tsx
@@ -4,20 +4,57 @@ import Background from "@/components/Background/Background";
 import Navigator from "@/components/Navigator/Navigator";
 import Tools from "@/components/Tools/Tools";
 import ScrollBar from "@/components/ScrollBar/ScrollBar";
+import { getBlogUserInfo, getImageUrl } from "@/services/webService";
 import "./BlogLayout.scss";
 
 const BlogLayout: FC = () => {
     const [navIndex, setNavIndex] = useState<number>(1);
-    const [bgImage] = useState<string>("https://easy-blog-test.oss-cn-guangzhou.aliyuncs.com/images/background_image.webp");
+    const [userName, setUserName] = useState<string>("Blog");
+    const [bgImage, setBgImage] = useState<string>("");
 
     useEffect(() => {
-        document.title = "Tianyi's Blog";
+        // 获取用户信息
+        const fetchUserInfo = async () => {
+            try {
+                const userInfo = await getBlogUserInfo();
+                if (userInfo) {
+                    // 设置用户名
+                    setUserName(userInfo.user_name || "H2Blog");
+                    
+                    // 设置背景图片
+                    if (userInfo.background_image) {
+                        setBgImage(getImageUrl(userInfo.background_image));
+                    }
+                    
+                    // 设置网站标题
+                    document.title = `${userInfo.user_name}'s Blog`;
+                    
+                    // 设置网站图标
+                    if (userInfo.web_logo) {
+                        const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+                        if (favicon) {
+                            favicon.href = getImageUrl(userInfo.web_logo);
+                        } else {
+                            // 如果不存在favicon，则创建一个
+                            const newFavicon = document.createElement('link');
+                            newFavicon.rel = 'icon';
+                            newFavicon.href = getImageUrl(userInfo.web_logo);
+                            document.head.appendChild(newFavicon);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error("获取用户信息失败:", error);
+            }
+        };
+
+        fetchUserInfo();
     }, []);
 
     return (
         <div className="blog-layout">
-            <Background backgroundImage={bgImage}/>
-            <Navigator className="blog-layout-navigator" index={navIndex} setIndex={setNavIndex}/>
+            {bgImage && <Background backgroundImage={bgImage}/>}
+            <Navigator className="blog-layout-navigator" index={navIndex} setIndex={setNavIndex} userName={userName}/>
             <div className="blog-content">
                 <Outlet/> {/* 这里会渲染子路由组件 */}
             </div>
